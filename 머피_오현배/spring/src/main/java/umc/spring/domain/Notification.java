df2dc4aabae1d2c2d4d6eee1ad5ad506eb437fcc
shihan00321 <shihan001@naver.com>
2023-11-26 15:35:34 +0900

    FEAT: API 응답 통일 & 에러핸들러 작성 및 8주차 미션 수행

---
+++ 머피_오현배/spring/src/main/java/umc/spring/domain/Notification.java
@@ -0,0 +1,30 @@
+package umc.spring.domain;
+
+import lombok.*;
+import umc.spring.domain.common.BaseEntity;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Notification extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Column(columnDefinition = "TINYINT(1) DEFAULT 0")
+    private boolean isConfirmed;
+
+    @Column(nullable = false, length = 50)
+    private String title;
+
+    @Column(nullable = false, length = 100)
+    private String description;
+}
